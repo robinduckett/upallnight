4e11824dd6ab349ca0fe340eac325873d275816f
Robin Duckett <robin.duckett@gmail.com>
2011-08-27 01:56:08 +0100

    Putting routes in a seperate file

--- app.js
+++ app.js
@@ -28,11 +28,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
-});
+require('./routes')(app);
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Ragechat server listening on port %d in %s mode", app.address().port, app.settings.env);
