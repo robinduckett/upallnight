18fae7af0d0d2466df5b3ab3dcf370e20b13573c
Robin Duckett <robin.duckett@gmail.com>
2011-08-28 03:55:30 +0100

    faces

---
+++ public/javascripts/faces.js
@@ -0,0 +1,7 @@
+var faces = [
+  'neutral1', 'neutral2', 'neutral4', 'neutral7',
+  'neutral12', 'neutral13', 'neutral15', 'neutral37',
+  'poker', 'badpokerface', 'retard', 'almost',
+  'almost2', 'thickpoker', 'richfu', 'thingswentokay',
+  'yawn'
+];
