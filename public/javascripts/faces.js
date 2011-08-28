5d9f26fc5e276032f2246d2f2a361380b6cb8cc0
tomaszjureczko <tomasz.jureczko@gmail.com>
2011-08-28 08:28:25 +0200

    Function to replace rageMarkdown with rage faces

--- public/javascripts/faces.js
+++ public/javascripts/faces.js
@@ -6,14 +6,14 @@ var faces = [
   'yawn'
 ];
 
-(function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
-
 var replaceFaces = function(element) {
     var toReplace, replaceWith,offsetRight,offsetTop;
     var chat = $(element);
-    for(face in faces) {    
-        replaceWith = "<img class='face'>";
-        toReplace = faces[face];
+    for(face in faces) {
+        offsetTop = -Math.floor(face / 4) * 64;
+        offsetRight = -(face % 4) * 64;
+        replaceWith = "<img class='face' style='background-position:" + offsetRight + "px " + offsetTop + "px'>";
+        toReplace = '[(]' + faces[face] + '[)]';
         chat.html(chat.html().replace(new RegExp(toReplace,"gi"), replaceWith)); 
     }
 }
