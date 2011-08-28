b20b37ffd4fc38747eb86e382cd42a54dc948880
tomaszjureczko <tomasz.jureczko@gmail.com>
2011-08-28 07:17:36 +0200

    Replacing strings with faces in chat

--- public/javascripts/faces.js
+++ public/javascripts/faces.js
@@ -5,3 +5,15 @@ var faces = [
   'almost2', 'thickpoker', 'richfu', 'thingswentokay',
   'yawn'
 ];
+
+(function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
+
+var replaceFaces = function(element) {
+    var toReplace, replaceWith,offsetRight,offsetTop;
+    var chat = $(element);
+    for(face in faces) {    
+        replaceWith = "<img class='face'>";
+        toReplace = faces[face];
+        chat.html(chat.html().replace(new RegExp(toReplace,"gi"), replaceWith)); 
+    }
+}
